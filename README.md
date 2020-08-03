bef0128a2be8903e2b2c90afc22f48dc83fdb2d5
Andrew Bales <agbales@gmail.com>
2020-08-02 17:32:37 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Near earth object tracker
+
+Using NASA's NeoWs (Near Earth Object Web Service) to give a list of all objects passing by earth on any given day. Pick a date and see what's coming our way.
+
+This project was created as part of a learning series I'm hosting for coders coming from the humanities and arts.
+
+Live website:
+Data: [NASA](https://api.nasa.gov/#:~:text=NeoWs%20(Near%20Earth%20Object%20Web,browse%20the%20overall%20data%2Dset.)
---
+++ src/index.js
@@ -0,0 +1,40 @@
+const apiKey = config.API_KEY;
+
+function renderPage(date) {
+  const url = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${date}&end_date=${date}&api_key=${apiKey}`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("app").innerHTML = "";
+      addDataToDom(data, date);
+    })
+    .catch((error) => console.log(error));
+
+  function addDataToDom(dataObject, day) {
+    const asteroids = dataObject.near_earth_objects;
+    const arrayData = asteroids[day];
+
+    arrayData.forEach((entry) => {
+      const approachData = entry.close_approach_data[0];
+      const averageDiameter =
+        entry.estimated_diameter.meters.estimated_diameter_min +
+        entry.estimated_diameter.meters.estimated_diameter_max / 2;
+
+      const element = `<div>
+        <h3>${entry.name}</h3>
+        <p>Miss distance: ${approachData.miss_distance.lunar} earth-moon hwys away</p>
+        <p>Diameter (maybe?): ${averageDiameter} meters</p>
+        <p>Dangerous?: ${entry.is_potentially_hazardous_asteroid}</p>
+      </div>`;
+      document.getElementById("app").innerHTML += element;
+    });
+  }
+}
+
+function updateData() {
+  const date = document.getElementById("date-input").value;
+  renderPage(date);
+}
+
+document.getElementById("date-update").addEventListener("click", updateData);
